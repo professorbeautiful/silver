17324d65f6609eebd27df2f6d8f2f162495e2785
Roger Day <notsofeo@gmail.com>
2019-07-04 10:17:19 -0400

    Improved graph of dates.

--- analyses-by-date.R
+++ analyses-by-date.R
@@ -2,20 +2,32 @@ allyears = as.numeric(sort(unique(years)))
 
 table(years)
 
-table(mammaprint_in_Ti, oncotype_in_Ti)
-
-plot(allyears, table(years), type='h' )
+table(mammaprint_in_TiAbKw, oncotype_in_TiAbKw)
 
 table(match(years, allyears))
 
-for(year in allyears)
+plot(allyears, as.vector(table(years)), type='h', col='lightgrey' )
+offset=0.2
+lwd = 2
+for(year in allyears) {
   points(type='h',
-  0.1+year, sum(years[mammaprint_in_TiAb]==year),
-      col='red')
-for(year in allyears)
+         offset+year, sum(years[mammaprint_in_TiAbKw&!oncotype_in_TiAbKw]==year),
+         col='red', lwd=lwd)
   points(type='h',
-    -0.1+year, sum(years[oncotype_in_TiAb]==year),
-    col='green')
+         -offset+year, sum(years[oncotype_in_TiAbKw&!mammaprint_in_TiAbKw]==year),
+         col='green', lwd=lwd)
+  points(type='h',
+         year, sum(years[mammaprint_in_TiAbKw&oncotype_in_TiAbKw]==year),
+         col='blue', lwd=lwd)
+  lines(c(year-offset,year+offset), c(0,0))
+}
+legendColors = c('green', 'red', 'blue', 'grey')
+legend('topleft',
+       legend=c('ODX', 'MP', 'both', 'total') , 
+       lwd=lwd,
+       text.col=legendColors,
+       col=legendColors
+)
 
-sort(unique(years[mammaprint_in_TiAb]))
-sort(unique(years[oncotype_in_TiAb]))
+
+#### why 2002 and 2020?
