650d8b0b4986815fbd73b6524bce1b9ac904aacc
Roger Day <notsofeo@gmail.com>
2019-07-04 17:16:33 -0400

    plotYears: Plotting the pmid's sampled.

--- analyses-by-date.R
+++ analyses-by-date.R
@@ -6,28 +6,37 @@ table(mammaprint_in_TiAbKw, oncotype_in_TiAbKw)
 
 table(match(years, allyears))
 
-plot(allyears, as.vector(table(years)), type='h', col='lightgrey' )
-offset=0.2
-lwd = 2
-for(year in allyears) {
-  points(type='h',
-         offset+year, sum(years[mammaprint_in_TiAbKw&!oncotype_in_TiAbKw]==year),
-         col='red', lwd=lwd)
-  points(type='h',
-         -offset+year, sum(years[oncotype_in_TiAbKw&!mammaprint_in_TiAbKw]==year),
-         col='green', lwd=lwd)
-  points(type='h',
-         year, sum(years[mammaprint_in_TiAbKw&oncotype_in_TiAbKw]==year),
-         col='blue', lwd=lwd)
-  lines(c(year-offset,year+offset), c(0,0))
+plotYears = function(subset) {
+  if(missing(subset)) subset = pmid
+  inSubset = pmid %in% subset
+  plot(allyears, as.vector(table(years[inSubset])),
+       type='h', col='lightgrey' )
+  offset=0.2
+  lwd = 2
+  
+  for(year in allyears) {
+    points(type='h',
+           offset+year, 
+           sum(years[mammaprint_in_TiAbKw&!oncotype_in_TiAbKw&inSubset]==year),
+           col='red', lwd=lwd)
+    points(type='h',
+           -offset+year, sum(years[oncotype_in_TiAbKw&!mammaprint_in_TiAbKw&inSubset]==year),
+           col='green', lwd=lwd)
+    points(type='h',
+           year, sum(years[mammaprint_in_TiAbKw&oncotype_in_TiAbKw&inSubset]==year),
+           col='blue', lwd=lwd)
+    lines(c(year-offset,year+offset), c(0,0))
+  }
+  legendColors = c('green', 'red', 'blue', 'grey')
+  legend('topleft',
+         legend=c('ODX', 'MP', 'both', 'total') , 
+         lwd=lwd,
+         text.col=legendColors,
+         col=legendColors
+  )
 }
-legendColors = c('green', 'red', 'blue', 'grey')
-legend('topleft',
-       legend=c('ODX', 'MP', 'both', 'total') , 
-       lwd=lwd,
-       text.col=legendColors,
-       col=legendColors
-)
+plotYears()
+
 
 plot(ecdf(pubMedPubDate))
 
@@ -49,13 +58,16 @@ tapply(pmid, INDEX=list(mammaprint_in_TiAbKw,
                         oncotype_in_TiAbKw,
                         years), 
        FUN= head, n=1)
-MaxNumPMID = 10
 pmidSelector = function(X)
   sample(X, min(MaxNumPMID, length(X)), 
          replace = FALSE)
+MaxNumPMID = 8  #### 10->243 articles, 9->231, 8->218
 pmidSampleArray = tapply(pmid, INDEX=list(mammaprint_in_TiAbKw, 
                         oncotype_in_TiAbKw,
                         years), 
        FUN= pmidSelector)
-unlist(pmidSampleArray)
-
+pmidSample = unlist(pmidSampleArray)
+length(pmidSample)
+plotYears(pmidSample)
+abline(h=8, lty=2)
+title(paste('up to ', MaxNumPMID, ' per PMID group'))
