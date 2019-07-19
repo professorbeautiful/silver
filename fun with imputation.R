66ee73f783a6cd073ccf38accf1e85128f23e9d6
Roger Day <notsofeo@gmail.com>
2019-07-19 16:35:53 -0400

    A safer merging of full N=407 data with our dataForFocus. Using imputationMerge. Created function doImputation() for greater ease exploring models.

--- fun with imputation.R
+++ fun with imputation.R
@@ -9,9 +9,31 @@ dataForFocus = dataForFocus[dataForFocus$notRelevant == FALSE
 
 dim(dataForFocus)
 
-### check it!  
+### check matching with the titles ####  
 dataForFocus$PMID[dataForFocus$isEconomics]
-titles[pmid %in% dataForFocus$PMID[dataForFocus$isEconomics]]
+titles[pmid %in% dataForFocus$PMID[dataForFocus$isEconomics] ] 
+###  OK, it appears that we are selecting the titles correctly 
+### But the order is wrong:
+dataForFocus$PMID[dataForFocus$isEconomics] [1]
+pmid[pmid %in% dataForFocus$PMID[dataForFocus$isEconomics] ] [1]
+sort(pmid[pmid %in% dataForFocus$PMID[dataForFocus$isEconomics] ]) [1]
+####  Using imputationMerge defined below:
+imputationMerge$titles[sapply(imputationMerge$isEconomics, isTRUE)]
+sort(imputationMerge$titles[sapply(imputationMerge$isEconomics, isTRUE)]) ==
+  sort(titles[pmid %in% dataForFocus$PMID[dataForFocus$isEconomics] ] )
+####  OK we're all set now.  Remember to use isTRUE for booleans:  isTRUE(NA) is FALSE! ####
+
+table(years[pmid %in% dataForFocus$PMID[dataForFocus$isEconomics]])  ### First one in 2005
+table(oncotype_in_TiAbKw[pmid %in% dataForFocus$PMID[dataForFocus$isEconomics]],
+      mammaprint_in_TiAbKw[pmid %in% dataForFocus$PMID[dataForFocus$isEconomics]],
+      dnn = c('ODX', 'MP')
+)  
+
+#### so why is this wrong?  ####
+table(years[pmid %in% dataForFocus$PMID], dataForFocus$FOCUS)
+head(cbind(pmid[pmid %in% dataForFocus$PMID], dataForFocus$PMID) )  ### yup, wrong! 
+
+
 
 #### Modeling ####
 allFeatures = paste(names(dataForFocus[-(1:4)]), collapse=' + ')
@@ -33,14 +55,15 @@ imputationMerge =   merge(all = TRUE,
 )
 table(imputationMerge$isEconomics, exclude=NULL)
 nrow(imputationMerge)
+IR.df$PMID = IR.df$pmid
+imputationMerge = merge(imputationMerge, IR.df)
 
 #### Begin imputation ####
-NReps = 10
-glmResults = 
-  lapply(1:NReps, 
-         function(ignoreMe,
-                  ourFormula=imputationMerge$imputedValuesForIsEconomics
-                  ~ as.numeric(years) # + oncotype_in_TiAbKw + mammaprint_in_TiAbKw,
+doImputation = function(NReps = 10, printMe = FALSE,
+                        ourFormula=imputedValuesForIsEconomics
+                        ~ as.numeric(years) + oncotype_in_TiAbKw + mammaprint_in_TiAbKw) {
+  glmResults = lapply(1:NReps, 
+         function(ignoreMe     
          ) {
            imputationMerge$imputedValuesForIsEconomics = 
              rbinom(length(predictedProbs), 1, predictedProbs)
@@ -56,10 +79,15 @@ glmResults =
            #               imputationMerge$isEconomics,
            #               exclude=NULL) 
            # )
-           glm.out = glm(ourFormula, family=binomial)   #### test interesting questions here.
-           print(summary(glm.out))
+           glm.out = glm(data=imputationMerge, formula = ourFormula, family=binomial)   #### test interesting questions here.
+           if(printMe) print(summary(glm.out))
+           glm.out
          }
   )
+  glmResults
+}
+glmResults = doImputation()
+dimnames(summary(glmResults[[1]])$coefficients)
 sapply(glmResults, function(result)
-  coef(result)[2, 'Estimate']
+  coef(summary(result))['as.numeric(years)', 'z value']
 )
