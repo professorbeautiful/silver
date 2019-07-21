2e3e9eda9d2f3b90b8920ecc91c9f4111bc4036c
Roger Day <notsofeo@gmail.com>
2019-07-21 14:51:39 -0400

    We are not fiddling with tm or deepnet for now.

--- get_ourNotes_focusWords.R
+++ get_ourNotes_focusWords.R
@@ -3,20 +3,21 @@
 
 #### fun with tm ####
 
-library(tm)
-a_corp = VCorpus(VectorSource(abstracts))
-a_corp = tm_map(a_corp, removeWords, stopwords("english"))
-dtm = DocumentTermMatrix(a_corp)
-findFreqTerms(dtm, 15)
-findAssocs(dtm, 'economics', 0.5)
-dtm = removeSparseTerms(dtm, 0.8)
-inspect(weightTf(removeSparseTerms(dtm, 0.8)))
-str(dtm)
-table(dtm$v)
-dtm[10,21:23]
-dimnames(dtm)[[2]][421:423]
+# library(tm)
+# a_corp = VCorpus(VectorSource(abstracts))
+# a_corp = tm_map(a_corp, removeWords, stopwords("english"))
+# dtm = DocumentTermMatrix(a_corp)
+# findFreqTerms(dtm, 15)
+# findAssocs(dtm, 'economics', 0.5)
+# dtm = removeSparseTerms(dtm, 0.8)
+# inspect(weightTf(removeSparseTerms(dtm, 0.8)))
+# str(dtm)
+# table(dtm$v)
+# dtm[10,21:23]
+# dimnames(dtm)[[2]][421:423]
+# 
+# library(deepnet)
 
-library(deepnet)
 library(googlesheets)
 ### See also our file "Abstract Isabel Rivera Plata.Rmd"
 sheetURL =   'https://docs.google.com/spreadsheets/d/141nlDIfnPdSvGSHgLPen9_QpVJlA0vwrvKc4y9fh6dc'
