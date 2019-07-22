49c5510172d8161b08e460383d9ccd88307bc02c
Roger Day <notsofeo@gmail.com>
2019-07-22 15:58:36 -0400

    Additions to "year and oncotype help predict FOCUS"

--- year and oncotype help predict FOCUS.R
+++ year and oncotype help predict FOCUS.R
@@ -12,22 +12,39 @@ loglm(   ~ FOCUS + oncotype_in_TiAbKw,
 #### Try to analyze each FOCUS separately ####
 focusCategories = unique(bigMerge$FOCUS)[-2]   ## Removing NA.
 bigMerge$years = as.numeric(bigMerge$years)
-allFocusModels = lapply(focusCategories, function(focus) {
+glmAfocus = function(focus, predictor = 'oncotype_in_TiAbKw * years') {
   print(focus)
-  try((glm( (FOCUS ==  focus)
-            ~  oncotype_in_TiAbKw * years,
+  try((glm( 
+  formula=(FOCUS %in%  focus)
+            ~  #oncotype_in_TiAbKw * 
+    years ,
             data=bigMerge,
-            subset= onlyOneTest & !is.na(bigMerge$FOCUS), 
+            subset= onlyOneTest & !is.na(bigMerge$FOCUS) & years < 2019, 
             family=poisson
   ))
   )
-})
+}
+allFocusModels = lapply(focusCategories, glmAfocus)
+names(allFocusModels) = focusCategories
 names(allFocusModels[[1]])
 sapply(allFocusModels, '[', i='deviance')
 sapply(allFocusModels, '[', i='null.deviance')
 allFocusAnovas = lapply(allFocusModels, anova)
 allFocusAnovas
+names(allFocusAnovas) = focusCategories
 allFocusSummaries = lapply(allFocusModels, summary)
 names(allFocusSummaries) = focusCategories
+allFocusSummaries
+Pvalues = sapply(allFocusSummaries, function(summ) summ$coefficients[-1, 4])
+min(Pvalues)
+####  No relationships anywhere with interaction model####
 
-####  No relationships anywhere ####
+
+summary(glmAfocus(patientBehaviorFocus))
+summary(glmAfocus(patientOutcomeFocus))
+
+## Modeling ONLY with years, Patient prognosis gives P=0.013, est= -0.127
+allFocusModels[1]
+sort(sapply(allFocusModels, function(model) model$coefficients[2]))
+
+table(focusCategories)
