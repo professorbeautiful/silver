7e24157d713fff88e2f9135eb1b66d18dd87806e
Roger Day <notsofeo@gmail.com>
2019-07-23 10:57:09 -0400

    effect measure table for patient-oriented.

--- EffectMeasures.R
+++ EffectMeasures.R
@@ -3,12 +3,24 @@
 effectMeasureNames = fieldChoices$`Effect measures`
 
 hasEffectMeasure = function(EM)
-  sapply((ourNotes$`Effect measure\n#1`==EM) |
-           (ourNotes$`Effect measure\n#2`==EM) |
-           (ourNotes$`Effect measure\n#3`==EM),
+  sapply((ourNotes$`Effect measure\n#1`  %in%  EM) |
+           (ourNotes$`Effect measure\n#2`   %in%  EM) |
+           (ourNotes$`Effect measure\n#3`   %in%  EM),
          isTRUE)
+
 isMoney = hasEffectMeasure('Money')
 table(isMoney)
 t(table(isMoney, ourNotes$FOCUS))
+
+table(
+  hasEffectMeasure(c(
+    "Positive predictive value",
+    "Negative predictive value",
+    "Number needed to treat (NNT)",
+    "Net reclassification improvement",
+    "Recurrence free survival",
+    "Disease free survival")
+  )  
+)
 
 
