bbd1add6a4fad6d72ebd202d86755ae6d90dbf6b
Roger Day <notsofeo@gmail.com>
2019-07-23 11:33:36 -0400

    plotProportions

--- summaryTables.R
+++ summaryTables.R
@@ -3,9 +3,8 @@ discovery`)
 
 chisq.test(tableFV)
 
-tableOMV = table(ourNotes$`Oncotype DX?
-Mammaprint?`, ourNotes$`Validation/ 
-discovery`)
+tableOMV = table(ourNotes$`Oncotype DX?\nMammaprint?`, 
+                 ourNotes$`Validation/ \ndiscovery`)
 
 chisq.test(tableOMV)
 round(digits=1,chisq.test(tableOMV)$stdres)
@@ -25,3 +24,79 @@ doctorBehaviorFocus = fieldChoices$`FOCUS (primary endpoint)`[c(2,3,4,17)]
 table(ourNotes$FOCUS %in% doctorBehaviorFocus, 
       ourNotes$`Study\nsub-type` %in% computationSubTypes, 
       dnn = c('doctorBehaviorFocus', 'computation'))
+
+#### proportion doctorBehaviorFocus by year
+length(pmidSample)
+pmidSampleRelevant = ourNotes$PMID
+ourNotes = merge(ourNotes, data.frame(PMID=pmid, year=years))
+
+plotProportions = function(subset, feature=doctorBehaviorFocus, 
+                           summaryFunction=mean,
+                     .ylab='proportion', 
+                     legendLocation = 'topleft',
+                     ...) {
+   featureName = deparse(substitute(feature))
+   print(featureName)
+   if(missing(subset)) subset = pmid
+   inSubset = pmid %in% subset
+   ### we need to omit 2019. not complete.
+   featureByYear = split(ourNotes$FOCUS %in% feature, 
+                                     ourNotes$year)
+   
+   featureProportion = sapply(featureByYear, mean)
+   plot(allyears, featureProportion, 
+        type='h', col='lightgrey' ,
+        xlab='year', ylab=.ylab,
+        ylim = c(0,1), 
+        ...
+   )
+   title(featureName)
+   lines(allyears,
+         inverseLogit(
+            predict(glm(featureProportion ~ as.numeric(names(featureProportion)),
+                        family=binomial))
+         ),
+         col='lightgrey'
+   )
+   featureByYearAndTest = split(ourNotes$FOCUS %in% feature, 
+                         list(ourNotes$year, 
+                              ourNotes$`Oncotype DX?\nMammaprint?`)
+   )
+   
+   featureProportionByYearAndTest = sapply(featureByYearAndTest, mean)
+   
+   lwd = 2
+   offset=0.2
+   for(year in ouryears) {
+      for(test in unique(ourNotes$`Oncotype DX?\nMammaprint?`)) {
+         if(is.na(test)) break
+         thisData = featureProportionByYearAndTest[[paste(sep='.',
+                                   year,test)]]
+         color = switch(test, 
+                        "Mammaprint only (70 genes)" = 'red',
+                        "Oncotype only (21 genes)"  = 'green',
+                        "Both tests" = 'blue')
+         thisOffset = offset * 
+            switch(test, 
+                   "Mammaprint only (70 genes)" = +1,
+                   "Oncotype only (21 genes)"  = -1,
+                   "Both tests" = 0)
+         points(type='h',
+                thisOffset+year, 
+                mean(thisData),
+                col=color, lwd=lwd)
+      }
+      lines(c(year-offset,year+offset), c(0,0))
+   }
+   legendColors = c('green', 'red', 'blue', 'grey')
+   legend(legendLocation,
+          legend=c('ODX', 'MP', 'both', 'total') , 
+          lwd=lwd,
+          text.col=legendColors,
+          col=legendColors
+   )
+}
+
+plotProportions()
+plotProportions(feature = patientBehaviorFocus)
+plotProportions(feature = patientOutcomeFocus, legendLocation='topright')
