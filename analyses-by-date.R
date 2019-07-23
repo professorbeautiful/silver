820368cb4576943757574cd02411c4bd2fb7945c
Roger Day <notsofeo@gmail.com>
2019-07-23 12:50:42 -0400

    Using ouryears instead of plotyears.    No 2019.

--- analyses-by-date.R
+++ analyses-by-date.R
@@ -6,28 +6,32 @@ table(mammaprint_in_TiAbKw, oncotype_in_TiAbKw)
 
 table(match(years, allyears))
 
-plotYears = function(subset) {
+ouryears = allyears[allyears<2019]
+plotYears = function(subset, summaryFunction=sum,
+                     .ylab='article count', ...) {
   if(missing(subset)) subset = pmid
   inSubset = pmid %in% subset
-  yeartable = sapply(allyears, function(y)
-    sum(years[inSubset] == y))
+  ### we need to omit 2019. not complete.
+  yeartable = sapply(ouryears, function(y)
+    summaryFunction(years[inSubset] == y))
   ## as.vector(table(years[inSubset]))
-  plot(allyears, yeartable,
+  plot(ouryears, yeartable,
        type='h', col='lightgrey' ,
-       xlab='year', ylab='article count')
+       xlab='year', ylab=.ylab,
+       ...)
   offset=0.2
   lwd = 2
   
-  for(year in allyears) {
+  for(year in ouryears) {
     points(type='h',
            offset+year, 
-           sum(years[mammaprint_in_TiAbKw&!oncotype_in_TiAbKw&inSubset]==year),
+           summaryFunction(years[mammaprint_in_TiAbKw&!oncotype_in_TiAbKw&inSubset]==year),
            col='red', lwd=lwd)
     points(type='h',
            -offset+year, sum(years[oncotype_in_TiAbKw&!mammaprint_in_TiAbKw&inSubset]==year),
            col='green', lwd=lwd)
     points(type='h',
-           year, sum(years[mammaprint_in_TiAbKw&oncotype_in_TiAbKw&inSubset]==year),
+           year, summaryFunction(years[mammaprint_in_TiAbKw&oncotype_in_TiAbKw&inSubset]==year),
            col='blue', lwd=lwd)
     lines(c(year-offset,year+offset), c(0,0))
   }
@@ -39,7 +43,7 @@ plotYears = function(subset) {
          col=legendColors
   )
 }
-
+plotYears()
 plot(ecdf(pubMedPubDate))
 
 
@@ -96,13 +100,15 @@ yearMerge = merge(
 dataForYearStudy = data.frame(count= unlist(yearMerge[-1]))
 dataForYearStudy$year = c(yearMerge$year,yearMerge$year)
 dataForYearStudy$test = rep(c('mp', 'odx'), each=17)
-dataForYearStudy
+dataForYearStudy = dataForYearStudy[dataForYearStudy$year<2019,]
 # lines(dataForYearStudy$year[dataForYearStudy$test=='mp'], 
 #       dataForYearStudy$count[dataForYearStudy$test=='mp'], col='red')
 # lines(dataForYearStudy$year[dataForYearStudy$test=='odx'], 
 #       dataForYearStudy$count[dataForYearStudy$test=='odx'], col='green')
-glm.out.dataForYearStudy = glm(data=dataForYearStudy,
-    count ~ year * test, family=poisson)
+glm.out.dataForYearStudy = glm(
+  data=dataForYearStudy,
+  subset= (year < 2019),
+  count ~ poly(degree = 2, year * test), family=poisson)
 summary(glm.out.dataForYearStudy)
 predictions = exp(predict(glm.out.dataForYearStudy))
 plotYears()
