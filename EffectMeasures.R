0d1097d2f425de292f1572c7ac947199c1808133
Roger Day <notsofeo@gmail.com>
2019-07-23 12:43:17 -0400

    Chart for effect measure usage.

--- EffectMeasures.R
+++ EffectMeasures.R
@@ -45,3 +45,47 @@ table(
   )  
 )
 
+EMusage = sort(decreasing = TRUE,
+  apply(
+    sapply( unique(fieldChoices$`Effect measures`), hasEffectMeasure),
+    2,
+    sum)
+)
+
+plot(EMusage, axes=F, xlab="")
+axis(2)
+numText = 9
+offsetText = 5
+yFactor = 2
+yText = max(EMusage) - (-1 + 1:numText)* yFactor
+text(y = yText, x = (1:numText) + offsetText, names(EMusage) [1:numText],
+     pos=4, 
+     col=c('black', 'red') 
+     [1 + (names(EMusage)=='Net reclassification improvement')]
+     )
+for(i in 1:numText) lines(x=c(i, i+offsetText), y=c(EMusage[i], yText[i]),
+                          col=c('black', 'red') 
+                          [1 + (names(EMusage)[i]=='Net reclassification improvement')]
+)
+
+patientOrientedEM = c(    "Positive predictive value",
+      "Negative predictive value",
+      "Number needed to treat (NNT)",
+      "Net reclassification improvement"
+)
+
+points(which(names(EMusage) %in% patientOrientedEM), 
+       EMusage[which(names(EMusage) %in% patientOrientedEM)], 
+       col='red', pch=15)
+abline(h=0)
+text(x=1, y=2, 'predictive values', col='red', pos=4)
+lines(x=c(18, which(names(EMusage)=='Negative predictive value')), 
+          y=c(2, 1),
+      col='red' 
+)
+text(x=1, y=0.5, 'number needed to treat', col='red', pos=4)
+lines(x=c(24, which(names(EMusage)=='Number needed to treat (NNT)')), 
+      y=c(0.5, 0),
+      col='red' 
+)
+title("Effect measure usage\nPatient-oriented in red")
