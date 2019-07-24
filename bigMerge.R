dca5cc79446e5aa0b80a5fb69ed2534955c6cce6
Roger Day <notsofeo@gmail.com>
2019-07-23 21:21:34 -0400

    Adding in the ourNotes feature `Oncotype DX?\nMammaprint?` into bigMerge for non-ourNotes articles.

--- bigMerge.R
+++ bigMerge.R
@@ -12,4 +12,26 @@ ourNotesFeatures$isEconomics
 bigMerge =   merge(all = TRUE,
                    allArticles, ourNotesFeatures
 )
+table(bigMerge$oncotype_in_TiAbKw, bigMerge$mammaprint_in_TiAbKw, bigMerge$`Oncotype DX?
+Mammaprint?`, dnn=list('odx', 'mp', 'test'), exclude=NULL)
+### a few discrepancies:  7 out of 172.  (and 6 have some NA ?)
+### We won't worry about that now.
 
+bigMerge$`Oncotype DX?\nMammaprint?`[
+  is.na(bigMerge$`Oncotype DX?\nMammaprint?`) 
+  & bigMerge$mammaprint_in_TiAbKw 
+  & !bigMerge$oncotype_in_TiAbKw] = 
+  'Mammaprint only (70 genes)'
+
+bigMerge$`Oncotype DX?\nMammaprint?`[
+  is.na(bigMerge$`Oncotype DX?\nMammaprint?`) 
+  & !bigMerge$mammaprint_in_TiAbKw 
+  & bigMerge$oncotype_in_TiAbKw] = 
+  'Oncotype only (21 genes)'
+
+bigMerge$`Oncotype DX?\nMammaprint?`[
+  is.na(bigMerge$`Oncotype DX?\nMammaprint?`) 
+  & bigMerge$mammaprint_in_TiAbKw 
+  & bigMerge$oncotype_in_TiAbKw] = 
+  'Both tests'
+  
